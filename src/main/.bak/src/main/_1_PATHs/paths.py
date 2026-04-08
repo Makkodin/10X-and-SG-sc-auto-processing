82d007cbaff98446ca9726b07bfba59e7a920b10
Фатеев Олег Дмитриевич <ofateev@cs12.pak-cspmz.ru>
2026-04-08 07:40:24 +0000

    old

--- src/main/.bak/src/main/_1_PATHs/paths.py
+++
@@ -1,4 +0,0 @@
-bcl_load        =   '/mnt/cephfs3_ro/BCL/uvd*'
-fastq_load      =   '/mnt/cephfs*_ro/FASTQS/uvd*'
-refs_dir        =   '/mnt/raid0/ofateev/refs'
-soft_dir        =   '/mnt/raid0/ofateev/soft'
